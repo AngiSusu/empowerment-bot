18d4fa3496bc0d43bda8cfb0a47f42c5e6265205
Angelina Suwoto <tsarminas@gmail.com>
2019-02-16 17:42:47 -0500

    Nonfunctioning prelim

---
+++ Echobot.py
@@ -0,0 +1,16 @@
+from fbchat import log, Client
+
+# Subclass fbchat.Client and override required methods
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(author_id, thread_id)
+        self.markAsRead(author_id)
+
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If you're not the author, echo
+        if author_id != self.uid:
+            self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+
+client = EchoBot("lkw_63@yahoo.com", "pearlhacks2019")
+client.listen()
