2a37669a6be45d12a814920dcead9714c98a9d81
Sarah Chen <tuanyuan2008@dhcp-204-84-255-208.wireless-guest.unc.edu>
2019-02-16 20:39:20 -0500

    correct echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -1,16 +1,19 @@
+# -*- coding: UTF-8 -*-
+
 from fbchat import log, Client
 
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(author_id, thread_id)
-        self.markAsRead(author_id)
-
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
         log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
 
+        print author_id
+        print self.uid
         # If you're not the author, echo
         if author_id != self.uid:
             self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
-client = Client("pandadude8@hotmail.com", "guacamole")
+client = EchoBot('pandadude8@hotmail.com', 'guacamole')
 client.listen()
